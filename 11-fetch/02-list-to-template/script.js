73f02ab2ddf61a3b07305d28ae0142297347e31b
Michael <michaeltesfayg@gmail.com>
2021-10-21 10:54:27 +0200

    odifications &1 bug solved

--- 11-fetch/02-list-to-template/script.js
+++ 11-fetch/02-list-to-template/script.js
@@ -10,27 +10,26 @@
 // You will have time to focus on it later.
 
 (() => {
-    const tpl = document.querySelector("#tpl-hero");
-    const target = document.querySelector("#target");
-  
-    document.querySelector("#run").addEventListener("click", async () => {
-      const response = await fetch("http://localhost:3000/heroes");
-  
-      // your code here
-      const xMenList = await response.json();
-      xMenList.forEach((xMen) => {
-        const newTPL = tpl.content.cloneNode(true);
-    
-        let newName = newTPL.querySelector(".name");
-        let newAlterEgo = newTPL.querySelector(".alter-ego");
-        let newPowers = newTPL.querySelector(".powers");
-  
-        newAlterEgo.textContent = xMen.alterEgo;
-        newPowers.textContent = xMen.abilities;
-        newName.textContent = xMen.name;
-  
-        target.append(newTPL);
-      });
+  const tpl = document.querySelector("#tpl-hero");
+  const target = document.querySelector("#target");
+
+  document.querySelector("#run").addEventListener("click", async () => {
+    const response = await fetch("http://localhost:3000/heroes");
+
+    // your code here
+    const xMenList = await response.json();
+    xMenList.forEach((xMen) => {
+      const newTPL = tpl.content.cloneNode(true);
+
+      let newName = newTPL.querySelector(".name");
+      let newAlterEgo = newTPL.querySelector(".alter-ego");
+      let newPowers = newTPL.querySelector(".powers");
+
+      newAlterEgo.textContent = xMen.alterEgo;
+      newPowers.textContent = xMen.abilities; //before this property name was saved us "powers"
+      newName.textContent = xMen.name;
+
+      target.append(newTPL);
     });
-  })();
-  
+  });
+})();
--- 11-fetch/04-add/script.js
+++ 11-fetch/04-add/script.js
@@ -10,49 +10,54 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
-  
-    const recievedName = document.getElementById("hero-name");
-    const recievedAlterEgo = document.getElementById("hero-alter-ego");
-    const recievedPower = document.getElementById("hero-powers");
-    document.getElementById("run").addEventListener("click", () => {
-      // const response= await fetch("http://localhost:3000/heroes");
-      // const heroesList= await response.json();
-  
-      const url = "http://localhost:3000/heroes";
-  
-      let userObject = {};
-      if (
-        recievedName.value === "" &&
-        recievedAlterEgo.value === "" &&
-        recievedPower.value === ""
-      ) {
-        console.log(
-          "Nothing recieved \nTry to fill all the submission form fileds"
-        );
-      } else if (
-        recievedName.value === "" ||
-        recievedAlterEgo.value === "" ||
-        recievedPower.value === ""
-      ) {
-        console.log(
-          "Ooops! \nat list on of the submission form fileds is not completed "
-        );
-      } else {
-        userObject.name = recievedName.value;
-        userObject.alterEgo = recievedAlterEgo.value;
-        userObject.power = recievedPower.value.split(","); //spliting the userinput(string) by comma & qotation
-        fetch(url, {
-          method: "post",
-          body: JSON.stringify(userObject),
-          headers: {
-            "content-type": "application/json; charset=UTF-8",
-          },
-        })
-          .then((response) => response.json())
-  
-          .then((json) => console.log(json));
-      }
-    });
-  })();
-  
+  // your code here
+
+  const recievedName = document.getElementById("hero-name");
+  const recievedAlterEgo = document.getElementById("hero-alter-ego");
+  const recievedPower = document.getElementById("hero-powers");
+  document.getElementById("run").addEventListener("click", async () => {
+    // const response= await fetch("http://localhost:3000/heroes");
+    // const heroesList= await response.json();
+
+    const url = "http://localhost:3000/heroes";
+
+    let userObject = {};
+    if (
+      recievedName.value === "" &&
+      recievedAlterEgo.value === "" &&
+      recievedPower.value === ""
+    ) {
+      console.log(
+        "Nothing recieved \nTry to fill all the submission form fileds"
+      );
+    } else if (
+      recievedName.value === "" ||
+      recievedAlterEgo.value === "" ||
+      recievedPower.value === ""
+    ) {
+      console.log(
+        "Ooops! \nat list one of the submission form fileds is not completed "
+      );
+    } else {
+      userObject.name = recievedName.value;
+      userObject.alterEgo = recievedAlterEgo.value;
+      //before the "abilities" property was named  "powers" .changed to fix small bug in execercise 2
+      userObject.abilities = recievedPower.value.split(","); //spliting the userinput(string) by comma & qotation
+      fetch(url, {
+        method: "post",
+        body: JSON.stringify(userObject),
+        headers: {
+          "content-type": "application/json; charset=UTF-8",
+        },
+      });
+      //the next two lines(.then) have been replaced by async-await b/c it is much more cleaner
+      // .then((response) => response.json())
+
+      // .then((json) => console.log(json));
+
+      const getAll = await fetch(url);
+      const updatedList = await getAll.json();
+      console.log(updatedList);
+    }
+  });
+})();
